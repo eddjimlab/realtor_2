f48e3eecec220cdadefd8fdd7a5585410a699869
Eduard <eddjimlab@gmail.com>
2019-04-05 12:36:33 +0300

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var passport = require("passport");
+var User = require("../models/user");
 var middleware = require("../middleware");
 
 
